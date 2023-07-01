5da7f892ae3eef351c4ffac5c1b1fed54c76dc22
Andymarch83 <5657031@gmail.com>
2023-07-01 21:19:07 +0300

    add stealHealth in Rogue

--- src/Rogue.java
+++ src/Rogue.java
@@ -1,8 +1,13 @@
 public class Rogue extends Armed{
+    public int steal;
     public Rogue(String name) {
         super(name);
         setType("Rouge");
 
+    }
+    public String stealHealth(int steal){
+        this.setHealth(Integer.parseInt(getHealth() + steal));
+        return getHealth();
     }
     public String getInfo(){
         String inf = ("|" + getType() + "| " + getName() + "| " + getHealth() + "| " + getAttack() + "| " + getDefence() + "|");
